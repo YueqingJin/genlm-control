a600002deedbd9473e12818e6e8acba9eae1eaa3
Yueqing Jin <ni23364@bristol.ac.uk>
2025-07-10 14:34:43 +0100

    new new new

--- genlm/control/simulation.py
+++ genlm/control/simulation.py
@@ -52,8 +52,8 @@ def load_llama(
     # 选择设备
     if torch.cuda.is_available():
         device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
+    # elif torch.backends.mps.is_available():
+    #     device = "mps"
     else:
         device = "cpu"
 
